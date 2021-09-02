3d5fae357d0e24617317b17694faf3cace2153d2
James Odell <jpodell21@gmail.com>
2021-09-01 18:09:17 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(morgan('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+var MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost/fitness';
+mongoose.connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+});
+
+app.listen(PORT, function() {
+    console.log(`Listening on port ${PORT}`);
+});
