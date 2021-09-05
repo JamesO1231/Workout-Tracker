4d36b2c7a5de3fc73671e28ded5c29057a3230db
James Odell <jpodell21@gmail.com>
2021-09-05 10:18:53 -0600

    deploying

--- server.js
+++ server.js
@@ -11,7 +11,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/workout', { useNewUrlParser: true, useFindAndModify: false });
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 app.use(require('./routes/index'));
 app.use(require('./routes/api/apiRoutes'));
