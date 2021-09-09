f9db2524453b46c1d184bf92da1462abe7a70e4c
James Odell <jpodell21@gmail.com>
2021-09-08 22:07:31 -0600

    server fix

--- server.js
+++ server.js
@@ -11,15 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/workout',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true, useUnifiedTopology: true, });
 
 app.use(require('./routes/index.js'));
 app.use(require('./routes/api/apiRoutes.js'));
